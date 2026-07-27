b32a99db8a253d5d6d78294f3f3d6ea6cd801f72
Suryaprakash1007 <suryaprakash31052006@gmail.com>
2026-07-27 18:55:48 +0530

    fix: use env variable for public profile API URL to support production deployments

--- frontend/src/services/publicProfileService.js
+++ frontend/src/services/publicProfileService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // Since this is public, we don't use the authenticated api instance
-const API_URL = "http://localhost:8080/api/public/users";
+const API_URL = import.meta.env.VITE_API_BASE_URL 
+    ? `${import.meta.env.VITE_API_BASE_URL}/api/public/users` 
+    : "http://localhost:8080/api/public/users";
 
 export const getPublicProfile = async (username) => {
     const response = await axios.get(`${API_URL}/${username}`);
